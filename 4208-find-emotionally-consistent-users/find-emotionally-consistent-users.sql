e938ab56b03c3190d45a3b7301d86293a5499e1d
Swarn Suman <swarnsuman004@gmail.com>
2026-05-02 01:23:59 +0530

    Time: 299 ms (48.96%) | Memory: 0B (100.00%) - LeetSync

--- 4208-find-emotionally-consistent-users/find-emotionally-consistent-users.sql
+++ 4208-find-emotionally-consistent-users/find-emotionally-consistent-users.sql
@@ -1,23 +1,18 @@
 # Write your MySQL query statement below
-WITH ranked AS (
-    SELECT
-        user_id,
-        reaction,
-        COUNT(*) AS reaction_count,
-        SUM(COUNT(*)) OVER (PARTITION BY user_id) AS total_reactions,
-        RANK() OVER (
-            PARTITION BY user_id 
-            ORDER BY COUNT(*) DESC
-        ) AS rnk
+WITH temp AS(
+    SELECT *,
+    COUNT(user_id) OVER(PARTITION BY user_id) AS cnt,
+    COUNT(user_id) OVER(PARTITION BY user_id,reaction) AS reaction_cnt
     FROM reactions
-    GROUP BY user_id, reaction
 )
-SELECT
-    user_id,
-    reaction AS dominant_reaction,
-    ROUND(reaction_count / total_reactions, 2) AS reaction_ratio
-FROM ranked
-WHERE rnk = 1
-  AND total_reactions >= 5
-  AND reaction_count / total_reactions >= 0.60
-ORDER BY reaction_ratio DESC, user_id ASC;
+
+SELECT user_id, 
+       reaction AS dominant_reaction,
+       ROUND(reaction_cnt/cnt,2) AS reaction_ratio
+       FROM (
+    SELECT *,
+    ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY reaction_cnt DESC) AS rn
+    FROM temp
+) t
+WHERE rn = 1 AND cnt >= 5 AND reaction_cnt * 1.0 / cnt >= 0.6
+ORDER BY reaction_ratio DESC, user_id;
