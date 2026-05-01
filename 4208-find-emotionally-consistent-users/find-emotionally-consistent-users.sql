e12badd7ff86236f5841f68be9512bafd0fccf5d
Swarn Suman <swarnsuman004@gmail.com>
2026-05-02 01:28:00 +0530

    Time: 314 ms (28.27%) | Memory: 0B (100.00%) - LeetSync

--- 4208-find-emotionally-consistent-users/find-emotionally-consistent-users.sql
+++ 4208-find-emotionally-consistent-users/find-emotionally-consistent-users.sql
@@ -9,10 +9,10 @@ WITH temp AS(
 SELECT user_id, 
        reaction AS dominant_reaction,
        ROUND(reaction_cnt/cnt,2) AS reaction_ratio
-       FROM (
+FROM (
     SELECT *,
     ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY reaction_cnt DESC) AS rn
     FROM temp
 ) t
-WHERE rn = 1 AND cnt >= 5 AND reaction_cnt * 1.0 / cnt >= 0.6
+WHERE rn = 1 AND cnt >= 5 AND reaction_cnt/cnt >= 0.6
 ORDER BY reaction_ratio DESC, user_id;
